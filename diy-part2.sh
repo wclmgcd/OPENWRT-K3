b8ceee557963d846da5e6cc4653934ba9e3a81d9
wclmgcd <13980437+wclmgcd@users.noreply.github.com>
2020-09-02 19:53:31 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,3 +13,4 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 git clone https://github.com/fw876/helloworld.git package/lean/luci-app-ssr-plus
+git clone https://github.com/yiguihai/luci-app-k3screenctrl package/lean/luci-app-k3screenctrl
