de2ba8b089e1da5304b49f839338f8da3c8c2cbd
wclmgcd <13980437+wclmgcd@users.noreply.github.com>
2020-09-03 17:34:21 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,7 +14,7 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 #rm -rf package/lean/k3screenctrl
 git clone https://github.com/fw876/helloworld.git package/lean/luci-app-ssr-plus
-mkdir package/k3
+#mkdir package/k3
 git clone https://github.com/lwz322/luci-app-k3screenctrl.git package/k3/luci-app-k3screenctrl
 git clone https://github.com/lwz322/k3screenctrl_build.git package/k3/k3screenctrl_build
 #git clone https://github.com/lwz322/k3screenctrl.git package/lean/k3screenctrl
