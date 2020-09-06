e6d15070a0db015b6e2610f08c00ed5c309f6d27
wclmgcd <13980437+wclmgcd@users.noreply.github.com>
2020-09-06 19:17:37 +0800

    Update k3-part2.sh

--- k3-part2.sh
+++ k3-part2.sh
@@ -14,7 +14,7 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 #rm -rf package/lean/k3screenctrl
 git clone https://github.com/fw876/helloworld.git package/lean/luci-app-ssr-plus
-git clone https://github.com/wclmgcd/luci-app-koolproxy.git package/lean/luci-app-koolproxy
+git clone https://github.com/wclmgcd/luci-app-koolproxyR.git package/lean/luci-app-koolproxyR
 #bash package/lean/luci-app-koolproxy/koolproxyupdate.sh
 #mkdir package/k3
 git clone https://github.com/lwz322/luci-app-k3screenctrl.git package/k3/luci-app-k3screenctrl
